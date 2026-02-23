3d4ce82a0d1a0291c88f2bbbe063dd290a228d43
umaxinternetservice-source <umaxinternetservice@gmail.com>
2026-02-24 00:27:14 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "<h1>Hi, I'm Sajjad Hossain 👋</h1><p>Welcome to my website!</p>"
+
+if __name__ == "__main__":
+    app.run(debug=True)

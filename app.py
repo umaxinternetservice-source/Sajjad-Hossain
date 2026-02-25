100103d82910a05aa030324b3626489ce8ac68b1
umaxinternetservice-source <umaxinternetservice@gmail.com>
2026-02-25 15:59:50 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,30 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-/*home*/
+#home
 @app.route("/")
 def home():
     return render_template("index.html", active_page="home")
-/*=about=*/
+#about
 @app.route("/about")
 def about():
     return render_template("about.html", active_page="about")
-/*=Projects=*/
+#Projects
 @app.route("/projects")
 def projects():
     return render_template("projects.html", active_page="projects")
-/*=photos=*/
+#photos
 @app.route("/photos")
 def projects():
     return render_template("photos.html", active_page="photos")
-/*=videos=*/
-@app.route("/projects")
+#videos
+@app.route("/videos")
 def projects():
-    return render_template("projects.html", active_page="projects")
-/*=contacts=*/
-if __name__ == "__main__":
-    app.run(debug=True)
+    return render_template("videos.html", active_page="videos")
+#contacts
+@app.route("/contacts")
+def projects():
+    return render_template("contacts.html", active_page="contacts")
 
 if __name__ == "__main__":
     app.run(debug=True)
